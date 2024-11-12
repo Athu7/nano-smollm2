d8732e87bdecae31e46a75e58b0587d80fd574bb
Atharva <atharva.inamdar@dataeaze.io>
2024-11-13 00:17:31 +0530

    add: attention implementation with test

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from dataclasses import dataclass
 
 @dataclass
@@ -11,7 +12,7 @@ class SmolLM2Config:
     n_head:int = 9
     n_kv_heads:int = 3
     norm_eps: float = 1e-05
-    dtype: torch.dtype = torch.bfloat16
+    dtype: torch.dtype = torch.float32
     rope_theta:int = 100000
     vocab_size: int = 49152    
 
@@ -32,3 +33,47 @@ def apply_rope(x:torch.Tensor, cos:torch.Tensor, sin:torch.Tensor):
     rotated = torch.cat((-x2, x1), dim = -1)
     roped = (x * cos[:T, :]) + (rotated * sin[:T, :])
     return roped.to(dtype=x.dtype)
+
+
+class MHA(nn.Module):
+    def __init__(self, config: SmolLM2Config):
+        super().__init__()
+        self.config = config
+        self.group_size = config.n_head // config.n_kv_heads
+        self.head_dim = config.n_embd // config.n_head
+        self.q_proj = nn.Linear(self.config.n_embd, self.config.n_embd, bias = False, dtype = config.dtype)
+        self.k_proj = nn.Linear(self.config.n_embd, self.config.n_kv_heads * self.head_dim, bias = False, dtype = config.dtype)
+        self.v_proj = nn.Linear(self.config.n_embd, self.config.n_kv_heads * self.head_dim, bias = False, dtype = config.dtype)
+        self.o_proj = nn.Linear(self.config.n_embd, self.config.n_embd, bias = False, dtype = config.dtype)
+        self.register_buffer("mask", torch.tril(torch.ones(1,1, self.config.block_size, self.config.block_size)))
+        cos, sin = pre_compute_rope(config = config)
+        self.register_buffer("sin", sin)
+        self.register_buffer("cos", cos)
+    
+    def forward(self, x:torch.Tensor):
+        B,T,C = x.shape
+        n_head = self.config.n_head
+        head_dim = self.config.n_embd // n_head
+        kv_head = self.config.n_kv_heads
+
+        # calculate q,k,v
+        q = self.q_proj(x).view(B,T,n_head, head_dim).transpose(1,2) # (B, n_head, T, head_dim)
+        k = self.k_proj(x).view(B,T,kv_head, head_dim).transpose(1,2) # (B, n_kv_heads, T, head_dim)
+        v = self.v_proj(x).view(B,T,kv_head, head_dim).transpose(1,2) # (B, n_kv_heads, T, head_dim)
+        
+        # rotate q and k 
+        q = apply_rope(q, sin = self.sin, cos = self.cos) 
+        k = apply_rope(k, sin = self.sin, cos = self.cos) 
+
+        # repeat the k and v groups 
+        k = k.repeat_interleave(self.group_size, dim = 1) # (B, n_kv_heads, T, head_dim) -> (B, n_head, T, head_dim)
+        v = v.repeat_interleave(self.group_size, dim = 1) # (B, n_kv_heads, T, head_dim) -> (B, n_head, T, head_dim)
+
+        attn_out = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p= 0, is_causal=True)
+        # attn_scores = (q @ k.transpose(-1, -2)) * (1.0 / math.sqrt(k.size(-1)))
+        # attn_scores = torch.masked_fill(attn_scores, self.mask[:,:, :T,:T]== 0, float("-inf"))
+        # attn_scores = torch.nn.functional.softmax(attn_scores, dim = -1)
+        # attn_out = attn_scores @ v
+        attn_out = attn_out.transpose(1,2).contiguous().view(B,T,C)
+        attn_out = self.o_proj(attn_out)
+        return attn_out
---
+++ test_attention.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+from model import SmolLM2Config, pre_compute_rope, apply_rope, MHA
+from transformers.models.llama.modeling_llama import LlamaSdpaAttention, LlamaConfig
+
+
+    
+
+@torch.inference_mode()
+def test_attention_smollm2():
+    # initialize our attention
+    config = SmolLM2Config()
+    attn = MHA(config=config)
+
+    # initialize hf attention
+    smollm2_config_hf = LlamaConfig.from_pretrained("HuggingFaceTB/SmolLM2-135M-Instruct")
+    attn_hf = LlamaSdpaAttention(config = smollm2_config_hf)
+
+    # load same weights in both implementations
+    model_data = torch.load("model.pt")
+    hf_dic = {
+    "q_proj.weight": model_data.get("model.layers.0.self_attn.q_proj.weight"),
+    "k_proj.weight": model_data.get("model.layers.0.self_attn.k_proj.weight"),
+    "v_proj.weight": model_data.get("model.layers.0.self_attn.v_proj.weight"),
+    "o_proj.weight": model_data.get("model.layers.0.self_attn.o_proj.weight"),
+    }
+
+    dic = {
+        "q_proj.weight": model_data.get("model.layers.0.self_attn.q_proj.weight"),
+        "k_proj.weight": model_data.get("model.layers.0.self_attn.k_proj.weight"),
+        "v_proj.weight": model_data.get("model.layers.0.self_attn.v_proj.weight"),
+        "o_proj.weight": model_data.get("model.layers.0.self_attn.o_proj.weight"),
+        "mask": attn.mask,
+        "sin": attn.sin,
+        "cos": attn.cos
+    }
+
+    attn_hf.load_state_dict(hf_dic)
+    attn.load_state_dict(dic)
+    
+    # test our implementation
+    B,T,C = 1, config.block_size, config.n_embd
+    inp = torch.randn(B, T, C)
+    position_ids = torch.arange(T).unsqueeze(0)
+    attn_out_hf = attn_hf(inp, position_ids = position_ids)[0]
+    attn_out = attn(inp.to(dtype = config.dtype))
+    torch.testing.assert_close(attn_out_hf, attn_out)
